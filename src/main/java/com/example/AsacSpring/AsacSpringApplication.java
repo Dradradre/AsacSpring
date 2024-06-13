58320d766a7357dd2655a1b1c3324840b29ec1ed
Dradradre <leeyonghak13@gmail.com>
2024-06-13 10:16:47 +0900

    init

---
+++ src/main/java/com/example/AsacSpring/AsacSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.AsacSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsacSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsacSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AsacSpring/AsacSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AsacSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsacSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
